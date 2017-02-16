1199e969af7fad2f888bf532c0a82559d246b71d
sbgraves237 <spencer.graves@effectivedefense.org>
2017-02-16 14:15:16 -0600

    add dir examples

--- searchPkgs.R
+++ searchPkgs.R
@@ -1,3 +1,11 @@
 getwd()
 
 help(pac=rmdshower)
+
+(rmdshowerDir <- system.file(package='rmdshower'))
+dir(rmdshowerDir)
+
+(rmdshowerEx <- system.file('examples',
+                            package='rmdshower'))
+dir(rmdshowerEx)
+
